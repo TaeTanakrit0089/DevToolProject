16b090749e5956bc07fac42538b3808bf45c9885
Tae <65070089@kmitl.ac.th>
2024-10-01 22:13:14 +0700

    Added hello world and Jenkinsfile

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
-if __name__ == '__main__':
-    print('Hello World!')
+import os
+
+
+def main():
+    # Displaying status
+    print("System Status:")
+    print("---------------")
+    print("Operating System:", os.name)
+    print("\n")
+    print("Environment Variables:", os.environ)
+    print("\n")
+    print("finished.")
+
+
+if __name__ == "__main__":
+    main()
