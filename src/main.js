bef31b8bb6e04193fe75aa878f6cfb02d89ea558
Tae <65070089@kmitl.ac.th>
2024-10-02 21:39:41 +0700

    Moved Frontend files location

--- src/main.js
+++
@@ -1,7 +0,0 @@
-// src/main.js
-import {createApp} from 'vue'
-import App from './App.vue'
-import './index.css'
-import '/src/css/font.css'
-
-createApp(App).mount('#app')
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: 'media', // or 'class' if you want to manually toggle
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
