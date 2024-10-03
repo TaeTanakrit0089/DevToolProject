2789ff5129a24d1bb447ad2f74dbe4880060d3e7
kasomsri <bess11234@hotmail.com>
2024-10-03 22:46:53 +0700

    DJANGO project

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM python:3.4
 
-RUN apt-get update
+RUN apt-get update \
+	&& apt-get install -y --no-install-recommends \
+		postgresql-client \
+	&& rm -rf /var/lib/apt/lists/*
 
 WORKDIR /usr/src/app
 RUN pip install django psycopg2 djangorestframework pygments python-dotenv
--- remindme/models.py
+++ remindme/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Family(models.Model):
+    name = models.CharField(max_length=200)
+    token = models.CharField(max_length=10) # TOKEN for Invite
+    user = models.ManyToManyField(User)
+    
+class Events(models.Model):
+    ROUTINE = {
+        0: 'once',
+        1: 'daily',
+        2: 'weekly',
+        3: 'monthly',
+        4: 'annually'
+    }
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    noti_date = models.DateField()
+    noti_time = models.TimeField()
+    routine = models.IntegerField(choices=ROUTINE, default=0)
+    family = models.ForeignKey(Family, null=True, on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
