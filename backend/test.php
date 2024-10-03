2de8e79f4bdbbf1ff4a0d286d684db50da025b3f
kasomsri <bess11234@hotmail.com>
2024-10-03 12:49:00 +0700

    test backend

--- backend/test.php
+++ backend/test.php
@@ -1,13 +1,13 @@
 <?php
-// $frontend_host = getenv('NODEJS_HOST');
-// $frontend_port = getenv('NODEJS_PORT');
-// $url = "http://{$frontend_host}:{$frontend_port}/api/endpoint";
+$frontend_host = getenv('NODEJS_HOST');
+$frontend_port = getenv('NODEJS_PORT');
+$url = "http://{$frontend_host}:{$frontend_port}/api/endpoint";
 
-// $ch = curl_init($url);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// $response = curl_exec($ch);
-// curl_close($ch);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
 
-// echo $response;
+echo $response;
 echo "eiei";
 ?>
