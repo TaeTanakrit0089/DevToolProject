a2c6f2a9741cd2d8e5371a0c0a8903d08507c7af
Sneezzzzzze <kimjunbeom1205@gmail.com>
2024-10-04 16:52:25 +0700

    MODIFTY start.sh

--- devtool_system/devtool_system/urls.py
+++ devtool_system/devtool_system/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("api.urls")),
-    path('remindme/', include("remindme.urls"))
+    path('remindme/', include("remindme.urls")),
+    path('authentication', include("django.contrib.auth.urls"))
 ]
