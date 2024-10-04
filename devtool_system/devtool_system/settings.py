793eeb82021ef7e9fac5ab797a0ff4b79f0f73a6
kasomsri <bess11234@hotmail.com>
2024-10-04 23:43:22 +0700

    test

--- devtool_system/devtool_system/settings.py
+++ devtool_system/devtool_system/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^97m*!sxi-ze6v++q(q32u*-b5&7!7*e48l(+vc!hh@2v*k6vx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False # True
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 # Application definition
