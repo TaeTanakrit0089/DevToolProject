9af297496144b63e1f148c3dc5c2ac6eed9cb743
Sneezzzzzze <kimjunbeom1205@gmail.com>
2024-10-04 16:36:06 +0700

    ADD Tailwind

--- devtool_system/remindme/views.py
+++ devtool_system/remindme/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django import views
+
 
 # Create your views here.
+
+class HomePage(views.View):
+    def get(self, request):
+        return render(request, 'index.html')
---
+++ devtool_system/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './*/templates/**/*.html'],
+    theme: {
+        extend: {
+            colors: {
+                white: "#ffffff",
+                black: "#0e0e11",
+                'brown': {
+                    50: '#faf7f6',
+                    100: '#f6eeed',
+                    200: '#ede0de',
+                    300: '#dec7c3',
+                    400: '#cba6a1',
+                    500: '#b5837e',
+                    600: '#9d6361',
+                    700: '#824e4e',
+                    800: '#6b4142',
+                    900: '#5f3c3e',
+                    950: '#331e1f',
+                },
+            }
+
+        }
+    },
+    plugins: [],
+}
