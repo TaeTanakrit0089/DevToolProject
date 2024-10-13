0499e14a10a0baba68a2ef7784f34846b1ba17db
Nuntapong <tumacsp@gmail.com>
2024-10-13 19:50:26 +0700

    COMPLETE audio

--- devtool_system/devtool_system/settings.py
+++ devtool_system/devtool_system/settings.py
@@ -17,6 +17,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+GOOGLE_TTS_API_KEY = os.getenv('GOOGLE_TTS_API_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
