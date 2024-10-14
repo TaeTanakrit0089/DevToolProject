df6039be83154af459f999d601d4b1018ddaf549
Tae <65070089@kmitl.ac.th>
2024-10-14 15:16:58 +0700

    Updated the homepage title and improved the start.sh script

--- devtool_system/start.sh
+++ devtool_system/start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
+python manage.py makemigrations
+python manage.py migrate
+
 # Wait for the NPM build to complete
 while [ ! -f /app/.tailwind_build_complete ]; do
   echo "Waiting for npm build to complete..."
   sleep 1
 done
-
-python manage.py makemigrations
-python manage.py migrate
 
 python manage.py collectstatic --no-input
 
